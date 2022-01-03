8bee66f9c3c0618d471a13b66f5a3a9c91d6ecc5
Léo Marché <leo.marche@chartreuse-gaming.fr>
2022-01-04 00:11:49 +0100

    working prototype, need to add tests

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
-	"crypto/sha256"
+	"flag"
 	"fmt"
+	"path"
+	"path/filepath"
 
-	"github.com/LeoMarche/filehasher/pkg/hasherutils"
+	"github.com/LeoMarche/filehasher/pkg/copyutils"
 )
 
 func main() {
-	h := sha256.New()
-	fmt.Println(hasherutils.HashFolder("./", h))
+	var src = flag.String("s", "-1", "the source directory")
+	var dst = flag.String("d", "-1", "the destination directory")
+	var tries = flag.Int("n", 5, "number of tries when copying")
+
+	flag.Parse()
+
+	foldersrc := path.Base(*src)
+	folderdst := path.Base(*dst)
+
+	if foldersrc != folderdst {
+		*dst = filepath.Join(*dst, foldersrc)
+	}
+
+	err := copyutils.CopyTree(*src, *dst, *tries)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Copy terminated with success !")
+	}
 }
